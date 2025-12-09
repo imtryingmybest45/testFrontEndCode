0e3ea299034da0d499eaab0d0c1e2b0aeb1320bc
imtryingmybest45 <sebokapril@gmail.com>
2025-12-09 11:00:06 -0500

    Add hellYeah

---
+++ src/pages/HerHeartIsBreaking.js
@@ -0,0 +1,16 @@
+function HerHeartIsBreaking(props){
+      const { name } = props; // Destructure the prop
+      return (
+        <div>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Release date: 2019
+
+Rating: 5/10
+
+Unfortunately, this movie was nothing to write home about. It kind of sucked, honestly. I dont know why it was so bad, but it was.
+
+My family just laughed at my vote.</p>
+        </div>
+      );
+    }
+
+export default HerHeartIsBreaking;
