3bda4bbe9a107577953ef2e1cffe4d795b282ae9
imtryingmybest45 <sebokapril@gmail.com>
2025-12-09 10:51:20 -0500

    Add hellYeah

---
+++ src/pages/ThisLoveHas.js
@@ -0,0 +1,10 @@
+function ThisLoveHas(props){
+      const { name } = props; // Destructure the prop
+      return (
+        <div>
+            <p style={{ whiteSpace: 'pre-wrap' }}>{name}</p>
+        </div>
+      );
+    }
+
+export default ThisLoveHas;
