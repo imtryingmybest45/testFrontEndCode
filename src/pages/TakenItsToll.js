67e12ba193cb6f5a489c1848b5c6ceea09003077
imtryingmybest45 <sebokapril@gmail.com>
2025-12-09 10:52:55 -0500

    Add hellYeah

---
+++ src/pages/TakenItsToll.js
@@ -0,0 +1,12 @@
+function TakenItsToll(props){
+      const { name } = props; // Destructure the prop
+      return (
+        <div>
+            <p style={{ whiteSpace: 'pre-wrap' }}>on me, she said goodbye
+
+Too many times before</p>
+        </div>
+      );
+    }
+
+export default TakenItsToll;
