aeb9ed6d1c11d90077672b23f6728abac610c3e1
imtryingmybest45 <sebokapril@gmail.com>
2025-12-09 11:03:26 -0500

    Add hellYeah

---
+++ src/pages/WhereAreUNow.js
@@ -0,0 +1,16 @@
+function WhereAreUNow(props){
+      const { name } = props; // Destructure the prop
+      return (
+        <div>
+            <p style={{ whiteSpace: 'pre-wrap' }}>That I need you
+<br/>>
+I was alone, and now I'm fine
+<br/>>
+What do u mean
+<br/>>
+bum bum bum</p>
+        </div>
+      );
+    }
+
+export default WhereAreUNow;
