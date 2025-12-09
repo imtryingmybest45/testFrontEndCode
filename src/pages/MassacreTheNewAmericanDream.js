24edb09c044e3c0d7b83073bbdb2867ec3bd6ee7
imtryingmybest45 <sebokapril@gmail.com>
2025-12-09 11:10:40 -0500

    Add hellYeah

---
+++ src/pages/MassacreTheNewAmericanDream.js
@@ -0,0 +1,19 @@
+function MassacreTheNewAmericanDream(props){
+      const { name } = props; // Destructure the prop
+      return (
+        <div>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Rating: 10/10
+<br /> 
+ <br />
+This song by Palaye Royale is amazing
+<br /> 
+ <br />
+ON YOUR KILLLING ON YOUR KILLING SPREE
+<br /> 
+ <br />
+Massacre the new american dream</p>
+        </div>
+      );
+    }
+
+export default MassacreTheNewAmericanDream;
