d03d8b2ba2fbf2aa51b8ec0c3de389697fed44e0
imtryingmybest45 <sebokapril@gmail.com>
2025-12-09 11:05:59 -0500

    Add hellYeah

---
+++ src/pages/WhereAreYouNowJBiebs.js
@@ -0,0 +1,14 @@
+function WhereAreYouNowJBiebs(props){
+      const { name } = props; // Destructure the prop
+      return (
+        <div>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Rating: 10/10
+<br />
+Opinion: Jbiebs is hot and awesome. So is his wife. Sucks he hates his wife, her makeup company is really freking dope.
+<br />
+Oh well.</p>
+        </div>
+      );
+    }
+
+export default WhereAreYouNowJBiebs;
