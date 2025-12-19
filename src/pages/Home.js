bbf0d13a22f3f38b4d61ac18ef7488c7e9d6238c
imtryingmybest45 <sebokapril@gmail.com>
2025-12-19 18:55:00 -0500

    Updated file via Java API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import TheAutopsyofJaneDoe2 from './TheAutopsyofJaneDoe2';
 import Conjuring2 from './Conjuring2';
 import Conjuring from './Conjuring';
 import ThisIsANewReview from './ThisIsANewReview';
@@ -33,6 +34,7 @@ function Home(props){
       { id: 3, text: 'This Is A New Review', to: '/ThisIsANewReview' },
       { id: 4, text: 'Conjuring', to: '/Conjuring' },
       { id: 5, text: 'Conjuring 2', to: '/Conjuring2' },
+      { id: 6, text: 'The Autopsy of Jane Doe 2', to: '/TheAutopsyofJaneDoe2' },
     ];
   }, []); // <-- Empty array ensures it's created only once
   const stvar = "hello";
@@ -51,6 +53,7 @@ function Home(props){
         <Route path="/ThisIsANewReview" element={<ThisIsANewReview name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/Conjuring" element={<Conjuring name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/Conjuring2" element={<Conjuring2 name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
+        <Route path="/TheAutopsyofJaneDoe2" element={<TheAutopsyofJaneDoe2 name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
       </Routes>
       <nav>
         <ul style={{listStyleType: 'none',  padding: "0", margin: "0"}}>
