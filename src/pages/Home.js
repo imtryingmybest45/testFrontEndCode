bb8b1683e888b7b1b1f0109614a33addc2c91883
imtryingmybest45 <sebokapril@gmail.com>
2025-12-20 13:35:22 -0500

    Updated file via Java API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import FunchytheSnowwoman from './FunchytheSnowwoman';
 import Insidious from './Insidious';
 import TheConjuringChapter2 from './TheConjuringChapter2';
 import {Routes} from 'react-router-dom';
@@ -25,6 +26,7 @@ function Home(props){
     return [
       { id: 1, text: 'The Conjuring Chapter 2', to: '/TheConjuringChapter2' },
       { id: 2, text: 'Insidious', to: '/Insidious' },
+      { id: 3, text: 'Funchy the Snowwoman', to: '/FunchytheSnowwoman' },
     ];
   }, []); // <-- Empty array ensures it's created only once
   const stvar = "hello";
@@ -40,6 +42,7 @@ function Home(props){
       <Routes>
         <Route path="/TheConjuringChapter2" element={<TheConjuringChapter2 name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/Insidious" element={<Insidious name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
+        <Route path="/FunchytheSnowwoman" element={<FunchytheSnowwoman name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
       </Routes>
       <nav>
         <ul style={{listStyleType: 'none',  padding: "0", margin: "0"}}>
