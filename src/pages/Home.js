f9e48dba7dfe7a8c39986401355bc772a903e50a
unknown <sebokapril@gmail.com>
2025-12-20 17:57:40 -0500

    hello

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,7 +21,6 @@ function Home(props){
   const linksData = useMemo(() => {
     return [
       { id: 1, text: 'The Conjuring Chapter 4', to: '/TheConjuringChapter4' },
-      { id: 2, text: 'The Conjuring Chapter 4', to: '/TheConjuringChapter4' },
     ];
   }, []); // <-- Empty array ensures it's created only once
   //const stvar = "hello";
@@ -35,7 +34,6 @@ function Home(props){
       {tfVar&&<p>This is where I review horror movies.</p>}
       {tfVar&&<p>Click on a movie name to see the review.</p>}
       <Routes>
-        <Route path="/TheConjuringChapter4" element={<TheConjuringChapter4 name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/TheConjuringChapter4" element={<TheConjuringChapter4 name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
       </Routes>
       <nav>
