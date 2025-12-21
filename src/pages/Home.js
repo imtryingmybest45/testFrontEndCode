92b0012fa81723ac2a515caa4110104de7ee85b1
unknown <sebokapril@gmail.com>
2025-12-20 20:04:22 -0500

    hello

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,5 @@
-import TheConjuring2 from './TheConjuring2';
 import Insidious from './Insidious';
-import IKnowWhatudid2 from './IKnowWhatudid2';
-import IKnowWhatudid from './IKnowWhatudid';
-import IKnowWhatudid3 from './IKnowWhatudid3';
-import FunchyThesnowWoman from './FunchyThesnowWoman';
-import FunchyThesnowWoman3 from './FunchyThesnowWoman3';
+import TheConjuring2 from './TheConjuring2';
 import {Routes} from 'react-router-dom';
 import {Route} from 'react-router-dom';
 import {Link} from 'react-router-dom';
@@ -26,13 +21,8 @@ function Home(props){
 
   const linksData = useMemo(() => {
     return [
-      { id: 4, text: 'Funchy The snowWoman 3', to: '/FunchyThesnowWoman3' },
-      { id: 5, text: 'Funchy The snowWoman', to: '/FunchyThesnowWoman' },
-      { id: 6, text: 'I Know What u did 3', to: '/IKnowWhatudid3' },
-      { id: 7, text: 'I Know What u did', to: '/IKnowWhatudid' },
-      { id: 8, text: 'I Know What u did 2', to: '/IKnowWhatudid2' },
-      { id: 9, text: 'Insidious', to: '/Insidious' },
-      { id: 10, text: 'The Conjuring 2', to: '/TheConjuring2' },
+      { id: 1, text: 'The Conjuring 2', to: '/TheConjuring2' },
+      { id: 2, text: 'Insidious', to: '/Insidious' },
     ];
   }, []); // <-- Empty array ensures it's created only once
   //const stvar = "hello";
@@ -46,13 +36,8 @@ function Home(props){
       {tfVar&&<p>This is where I review horror movies.</p>}
       {tfVar&&<p>Click on a movie name to see the review.</p>}
       <Routes>
-        <Route path="/FunchyThesnowWoman3" element={<FunchyThesnowWoman3 name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
-        <Route path="/FunchyThesnowWoman" element={<FunchyThesnowWoman name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
-        <Route path="/IKnowWhatudid3" element={<IKnowWhatudid3 name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
-        <Route path="/IKnowWhatudid" element={<IKnowWhatudid name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
-        <Route path="/IKnowWhatudid2" element={<IKnowWhatudid2 name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
-        <Route path="/Insidious" element={<Insidious name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/TheConjuring2" element={<TheConjuring2 name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
+        <Route path="/Insidious" element={<Insidious name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
       </Routes>
       <nav>
         <ul style={{listStyleType: 'none',  padding: "0", margin: "0"}}>
