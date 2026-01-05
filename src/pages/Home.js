802cc83099187c381ffcab7bf89133936562b9b8
imtryingmybest45 <aprilsebou@gmail.com>
2026-01-05 12:23:17 -0500

    multifile commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import AnnabelleCreation from './AnnabelleCreation';
-import BringHerBack from './BringHerBack';
 import Terrifier3 from './Terrifier3';
 import InsidiousChapter2 from './InsidiousChapter2';
 import TheDescent from './TheDescent';
@@ -33,7 +32,6 @@ function Home(props){
       { id: 3, text: 'The Descent', to: '/TheDescent', name: "The Descent" },
       { id: 5, text: 'Insidious Chapter 2', to: '/InsidiousChapter2', name: "Insidious Chapter 2" },
       { id: 6, text: 'Terrifier 3', to: '/Terrifier3', name: "Terrifier 3" },
-      { id: 7, text: 'Bring Her Back', to: '/BringHerBack', name: "Bring Her Back" },
       { id: 8, text: 'Annabelle: Creation', to: '/AnnabelleCreation', name: "Annabelle: Creation" },
     ];
   const linksDataSorted = [...linksDataUnsorted].sort((a, b) => {
@@ -59,7 +57,6 @@ function Home(props){
         <Route path="/TheDescent" element={<TheDescent name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/InsidiousChapter2" element={<InsidiousChapter2 name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/Terrifier3" element={<Terrifier3 name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
-        <Route path="/BringHerBack" element={<BringHerBack name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/AnnabelleCreation" element={<AnnabelleCreation name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
       </Routes>
       <nav>
