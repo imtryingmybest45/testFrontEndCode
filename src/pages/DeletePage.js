30a851b8fe428ed5795c211d41a9942a90525784
unknown <sebokapril@gmail.com>
2026-01-05 10:47:32 -0700

    hello

--- src/pages/DeletePage.js
+++ src/pages/DeletePage.js
@@ -17,6 +17,7 @@ function DeletePage(props){
     const yesClick = () => {
         setTFButton(false);
         setMess("Please wait, your request is pending.");
+        console.log(movieName);
         axios.post('https://testhelpme-cfh4afcpdreacnh8.canadacentral-01.azurewebsites.net/deleteEndpoint',movieName)
         //axios.post('http://localhost:8080/deleteEndpoint',movieName)
         .then(response => setMess(response.data))
