d9de00f7cdfd5099cb7a3299c6a8009954424ac3
imtryingmybest45 <aprilsebou@gmail.com>
2026-01-08 19:57:46 -0700

    multifile commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import Insidious from './Insidious';
 import InsidiousChapter10 from './InsidiousChapter10';
 import AsAboveSoBelow from './AsAboveSoBelow';
 import AnnabelleCreation from './AnnabelleCreation';
@@ -35,6 +36,7 @@ function Home(props){
       { id: 7, text: "Annabelle: Creation", to: "/AnnabelleCreation", name: "Annabelle: Creation" },
       { id: 8, text: "As Above So Below", to: "/AsAboveSoBelow", name: "As Above So Below" },
       { id: 9, text: "Insidious Chapter 10", to: "/InsidiousChapter10", name: "Insidious Chapter 10" },
+      { id: 10, text: "Insidious", to: "/Insidious", name: "Insidious" },
     ];
   const linksDataSorted = [...linksDataUnsorted].sort((a, b) => {
       return a.name.localeCompare(b.name);
@@ -61,6 +63,7 @@ function Home(props){
         <Route path="/AnnabelleCreation" element={<AnnabelleCreation name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/AsAboveSoBelow" element={<AsAboveSoBelow name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/InsidiousChapter10" element={<InsidiousChapter10 name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
+        <Route path="/Insidious" element={<Insidious name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
       </Routes>
       <nav>
         <ul style={{listStyleType: 'none',  padding: "0", margin: "0"}}>
