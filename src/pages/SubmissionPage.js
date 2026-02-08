d3e43a1ae4db0118d3fe90b50b43180c3eb4fcda
unknown <sebokapril@gmail.com>
2026-02-08 08:25:54 -0700

    wow

--- src/pages/SubmissionPage.js
+++ src/pages/SubmissionPage.js
@@ -56,8 +56,8 @@ function SubmissionPage(props){
         }
         else{
         setMess("Please wait, your review is loading.");
-        axios.post('https://testhelpme-cfh4afcpdreacnh8.canadacentral-01.azurewebsites.net/submitEndpoint',formData)
-        //axios.post('http://localhost:8080/submitEndpoint',formData)
+        //axios.post('https://testhelpme-cfh4afcpdreacnh8.canadacentral-01.azurewebsites.net/submitEndpoint',formData)
+        axios.post('http://localhost:8080/submitEndpoint',formData)
         .then(response => setMess(response.data))
         }
     };
