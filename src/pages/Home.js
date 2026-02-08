983965381f52d59ad8de97a997ddb14864a239ec
imtryingmybest45 <aprilsebou@gmail.com>
2026-02-08 10:39:39 -0500

    multifile commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import BlackPhone from './BlackPhone';
 import TheGirlNextDoor2007 from './TheGirlNextDoor2007';
 import Kafds from './Kafds';
 import BlackPhone2 from './BlackPhone2';
@@ -45,6 +46,7 @@ function Home(props){
       { id: 11, text: "Black Phone 2", to: "/BlackPhone2", name: "Black Phone 2", moviePoster: 'https://m.media-amazon.com/images/M/MV5BMTVjMzNmZGYtOWU5NS00NDYzLThhZTktZGNlODIwYWVhMDRmXkEyXkFqcGc@._V1_SX300.jpg', tier: 'B'},
       { id: 12, text: "Kafds", to: "/Kafds", name: "Kafds", moviePoster: 'error', tier: 'A'},
       { id: 13, text: "The Girl Next Door (2007)", to: "/TheGirlNextDoor2007", name: "The Girl Next Door (2007)", moviePoster: 'https://m.media-amazon.com/images/M/MV5BNTk4ZmI4YzctNTM5MC00YzY1LThjOTgtZjA5OTcwN2ZjNDFlXkEyXkFqcGdeQXVyMjE0MzE1MA@@._V1_SX300.jpg', tier: 'NO'},
+      { id: 14, text: "Black Phone", to: "/BlackPhone", name: "Black Phone", moviePoster: 'https://m.media-amazon.com/images/M/MV5BMjFhZTcxOTktMzllMS00MzIzLWJhODEtZDU5YTFkNzRjZWQyXkEyXkFqcGc@._V1_SX300.jpg', tier: 'A'},
     ];
   const linksDataSorted = [...linksDataUnsorted].sort((a, b) => {
       return a.name.localeCompare(b.name);
@@ -78,6 +80,7 @@ function Home(props){
         <Route path="/BlackPhone2" element={<BlackPhone2 name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/Kafds" element={<Kafds name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/TheGirlNextDoor2007" element={<TheGirlNextDoor2007 name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
+        <Route path="/BlackPhone" element={<BlackPhone name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
       </Routes>
       <nav>
         <ul style={{listStyleType: 'none',  padding: "0", margin: "0"}}>
