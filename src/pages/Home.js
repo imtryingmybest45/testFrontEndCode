1c6239e51cae74eb6fb1c5ed1823f2b28a05a736
unknown <sebokapril@gmail.com>
2026-04-10 13:25:51 -0400

    hello

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,28 +4,47 @@ import { Link, useLocation} from 'react-router-dom';
 import boolizardprev12 from './boolizardprev12.png';
 //import stopLizard from './stopLizard.png'
 
+function usePersistedState(key, defaultValue) {
+  // Initialize state with value from localStorage if available
+  const [state, setState] = useState(() => {
+    const storedValue = sessionStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : defaultValue;
+  });
+
+  // Update localStorage whenever the state changes
+  useEffect(() => {
+    sessionStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
+
+  return [state, setState];
+}
+
 function Home(props){
 
     let setUsersApp = props.setUsersApp;
     let setInfo = props.setInfo;
-    const [users, setUsers] = useState({}); // 1. Initialize empty array
-    const [sortedEntries, setSortedEntries] = useState([]);
+    //const [users, setUsers] = useState({}); // 1. Initialize empty array
+    const [users,setUsers] = usePersistedState('userState',{});
+    //const [sortedEntries, setSortedEntries] = useState([]);
+    const [sortedEntries, setSortedEntries] = usePersistedState('sortedState',[]);
     const location = useLocation();
 
     useEffect(()=>{
       if (Object.keys(users).length>0){
-      const sortedEntries = Object.entries(users).sort(([keyA], [keyB]) => keyA.localeCompare(keyB));
-      setSortedEntries(sortedEntries)}
+        const sortedEntries = Object.entries(users).sort(([keyA], [keyB]) => keyA.localeCompare(keyB));
+        setSortedEntries(sortedEntries)
+      }
       setUsersApp(users);
       localStorage.setItem('allData',JSON.stringify(users));
     },[users])
 
     useEffect(() => {
       if (typeof location.initVar === 'undefined'){
-      location.initVar = true;
-      //axios.get('http://localhost:8080/submitEndpoint')
-      axios.get('https://testhelpme-cfh4afcpdreacnh8.canadacentral-01.azurewebsites.net/submitEndpoint')
-      .then(response => setUsers(({...response.data})))
+        location.initVar = true;
+        sessionStorage.refreshVar = false; //added now
+        //axios.get('http://localhost:8080/submitEndpoint')
+        axios.get('https://testhelpme-cfh4afcpdreacnh8.canadacentral-01.azurewebsites.net/submitEndpoint')
+        .then(response => setUsers(({...response.data})))
       }
     }, []);
 
