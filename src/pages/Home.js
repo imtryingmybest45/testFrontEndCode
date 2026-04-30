ce908142345f91b3b9f7669bdba8054d3127813b
unknown <sebokapril@gmail.com>
2026-04-30 12:54:16 -0400

    ow

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useRef} from 'react';
 import { Link, useLocation} from 'react-router-dom';
+/*import React, { useCallback} from 'react';
+import { useFocusEffect, useIsFocused } from '@react-navigation/core'*/
 import boolizardprev12 from './boolizardprev12.png';
-//import stopLizard from './stopLizard.png'
-//{lizardVar && <img src = 'https://media.tenor.com/MI5DzBmG4V8AAAAM/tom-lizard.gif'/>}
 
 function usePersistedState(key, defaultValue) {
   // Initialize state with value from localStorage if available
@@ -20,17 +20,19 @@ function usePersistedState(key, defaultValue) {
   return [state, setState];
 }
 
+
 function Home(props){
 
     let setUsersApp = props.setUsersApp;
     let setInfo = props.setInfo;
-    //const [users, setUsers] = useState({}); // 1. Initialize empty array
     const [users,setUsers] = usePersistedState('userState',{});
-    //const [sortedEntries, setSortedEntries] = useState([]);
     const [sortedEntries, setSortedEntries] = usePersistedState('sortedState',[]);
     const location = useLocation();
-    //const [lizardVar, setLizardVar] = usePersistedState('lizard',true);
     const [lizardVar, setLizardVar] = usePersistedState('lizard',true);
+    const [query, setQuery] = useState("");
+    const [filteredUser, setFilteredUser] = useState([]);
+    const [isOpen, setIsOpen] = usePersistedState('howdy',false);
+    const menuRef = useRef(null);
 
     useEffect(()=>{
       if (Object.keys(users).length>0){
@@ -40,7 +42,7 @@ function Home(props){
       }
       setUsersApp(users);
       localStorage.setItem('allData',JSON.stringify(users));
-    },[users])
+      },[users])
 
     useEffect(() => {
       if (typeof location.initVar === 'undefined'){
@@ -49,13 +51,38 @@ function Home(props){
         //axios.get('http://localhost:8080/submitEndpoint')
         axios.get('https://testhelpme-cfh4afcpdreacnh8.canadacentral-01.azurewebsites.net/submitEndpoint')
         .then(response => setUsers(({...response.data})))
-        //.then(setLizardVar(false));
       }
     }, []);
+
+    useEffect(()=>{
+      //const filteredUser = Object.keys(users).filter(user => user.toLowerCase().startsWith(query));
+      const isBetter = query.length>0;
+      if (isBetter){
+      const filteredUser = Object.entries(users).filter(([key, value]) => key.toLowerCase().startsWith(query));
+      setFilteredUser(filteredUser);
+      setIsOpen(true);}
+    },[query]);
 
     const logClick = (value) => {
       setInfo(value)
     };
+
+    useEffect(() => {
+    const handleClickOutside = (event) => {
+      // If the menu is open and the clicked target is NOT inside the menu
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setIsOpen(false);
+        setQuery("");
+      }
+    };
+
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen]); // Only re-run if isOpen changes
 
     return (
     <div>
@@ -68,6 +95,17 @@ function Home(props){
       <ul style={{listStyleType: 'none',  padding: "0", margin: "0"}}>
         {sortedEntries.map(([key,value])=><li key={value.movieId}><Link key={value.movieId} to={'/TesterPage'} onClick={()=>logClick(value)}>{value.name}</Link></li>)}
       </ul>
+      <div className='top-right-class'>
+        {!lizardVar&& <input 
+            type="text" 
+            placeholder="Search..." 
+            value={query} 
+            onChange ={(e) => setQuery(e.target.value.toLowerCase())} 
+        />}
+        {isOpen && <ul ref={menuRef} style={{listStyleType: 'none',  padding: "0", margin: "0"}}>
+          {filteredUser.map(([key,value])=><li key={value.movieId}><Link key={value.movieId} to={'/TesterPage'} onClick={()=>logClick(value)}>{value.name}</Link></li>)}
+        </ul>}
+      </div>
     </div>
   );
 }
